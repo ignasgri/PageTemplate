c94cab27988b1f3f2d2a4a10a69bf12083b518b0
Ignas Grigaravicius <kickeris@gmail.com>
2018-09-24 20:25:32 +0100

    testing heroku

--- page_template/settings.py
+++ page_template/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['ignas-pagetemplate.herokuapp.com', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['ignas-pagetemplate.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
